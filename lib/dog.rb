7f4fb30ea029b23811c1e0abe1cfd11ab2b5f06b
Charlie Barth <charliepbarth@gmail.com>
2020-07-07 20:37:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 # Add your code here
 class Puppy
-  
+  @@all = []
+ 
+  attr_accessor :dog
+ 
+  def initialize(dog)
+    @dog = dog
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+end
